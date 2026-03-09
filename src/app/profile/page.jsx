d9ba3aac06254a8d29bf7b37e6f6d48fb2b4dd3d
kinleyp06 <kinleypem06@gmail.com>
2026-03-09 21:16:09 +0600

    sixth commit

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,8 +2,16 @@ export default function ProfilePage() {
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">User Profile</h1>
-            <div className="bg-gray-100 p-6 rounded-lg"></div> 
-            
+            <div className="bg-gray-100 p-6 rounded-lg">
+                <div className="border-2 border-dashed border-gray-300 p-12 text-center rounded-lg">
+                    <p className="Next-gray-500">
+                        Drag and drop a video file
+                    </p>
+                    <button className="mt-4 bg-red-500 text-white px-4 py-2 rounded">
+                        Select File
+                    </button>
+                </div>
+            </div>    
         </div>
-    )
+    );
 }
