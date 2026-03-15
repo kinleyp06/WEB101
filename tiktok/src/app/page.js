888f061ac53d46f22654a7748d3aa8b4bfa7eca8
kinleyp06 <kinleypem06@gmail.com>
2026-03-15 17:37:57 +0600

    commit

--- tiktok/src/app/page.js
+++ tiktok/src/app/page.js
@@ -1,8 +1,9 @@
+import VideoFeed from '../components/ui/VideoFeed';
+
 export default function Home() {
-  return (
-      <main className="flex-min-h-screen flex-col items-center justify-center p-24">
-        <h1 className="text-4xl font-bold">Tiktok</h1>
-        <p className="mt-4 text-xl">Welcome to our Tiktok project</p>
-      </main>
-  );
+    return (
+        <div className='py-4'>
+            <VideoFeed />
+        </div>
+    );
 }
